26bc61cc89a94b8f8709515ec8c9b2a9c22d7abc
Ektora <zsoltlucski@gmail.com>
2020-11-11 11:30:24 +0100

    Javascript dátum intervallum fejlesztése

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,26 @@
 window.onload=function(){
 	skillDate();
+	skillDateInterval();
 }
 function skillDate(){
-	var datum = new Date();
 	var elem = document.getElementById("skilldate");
-	var datum = (datum.getFullYear() + ". " + Number(datum.getMonth()+1) + " " +datum.getDate()+".");
+
+	var datum = new Date();
+	datum = (datum.getFullYear() + ". " + Number(datum.getMonth()+1) + " " +datum.getDate()+".");
+	elem.innerHTML = datum;
+}
+function skillDateInterval(){
+	var jelenDatum = new Date();
+	var kezdoDatum = new Date("2020-10-22");
+	var nap = jelenDatum - kezdoDatum;
+	nap=(Math.floor(nap/1000/60/60/24));
+	var ev = parseInt(nap/365);
+	nap =nap%365;
+	var ho = parseInt(nap/30);
+	nap = nap%30;
+	var elem = document.getElementById("skilldateinterval");
+	datum = " A honlapot fejlesztem:<br>" +
+	+ ev + " éve " + ho + " hónapja " + nap + " napja";
 	elem.innerHTML = datum;
 }
 function h3Szinez() {
