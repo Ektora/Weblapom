c6acc4305b33f4e164009eec7958ee03dbac46bd
Ektora <zsoltlucski@gmail.com>
2020-11-12 14:49:40 +0100

    Szintaktikai javítás a js fájlban + index fájl elkezdése

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,9 @@
 window.onload=function(){
-	skillDate();
 	skillDateInterval();
-}
+skillDate();
+};
 function skillDate(){
 	var elem = document.getElementById("skilldate");
-
 	var datum = new Date();
 	datum = (datum.getFullYear() + ". " + Number(datum.getMonth()+1) + " " +datum.getDate()+".");
 	elem.innerHTML = datum;
@@ -19,29 +18,13 @@ function skillDateInterval(){
 	var ho = parseInt(nap/30);
 	nap = nap%30;
 	var elem = document.getElementById("skilldateinterval");
-	datum = " A honlapot fejlesztem:<br>" +
-	+ ev + " éve " + ho + " hónapja " + nap + " napja";
+	elem.innerHTML = "Nem tudom mi a baj";
+	var datum = " A honlapot fejlesztem:<br>" + ev + " éve " + ho + " hónapja " + nap + " napja";
 	elem.innerHTML = datum;
-	if((nap%2)==0){
+	if((nap%2)===0){
 		elem.style.color = "#3D1864";
 	}
 	else{
 		elem.style.color = "#678A1F";
 	}
-}
-function h3Szinez() {
-    var randomszin;
-    var elem;
-    function randomszinez() {
-        var rnd = Math.random();
-        return Math.floor(rnd * 2);
-    }
-    randomszin = randomszinez();
-    elem = document.getElementById('valtszin');
-    if (randomszin < 1) {
-        elem.style.color = "#4b4b97";
-    }
-    else {
-        elem.style.color = "#5a1539";
-    }
 }
