b8ac0a75c6af431b1b76eb602e956a6783453f26
Ektora <zsoltlucski@gmail.com>
2020-11-18 14:59:03 +0100

    Tartalom feltöltése, about_me oldal responzivitásának növelése

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,12 +3,18 @@ window.onload=function(){
 skillDate();
 };
 function skillDate(){
+	try{
 	var elem = document.getElementById("skilldate");
 	var datum = new Date();
 	datum = (datum.getFullYear() + ". " + Number(datum.getMonth()+1) + " " +datum.getDate()+".");
 	elem.innerHTML = datum;
+	}
+catch(err){
+	alert("Baj van a skillDate() függvénnyel: " + err.message);
+}
 }
 function skillDateInterval(){
+	try{
 	var jelenDatum = new Date();
 	var kezdoDatum = new Date("2020-10-22");
 	var nap = jelenDatum - kezdoDatum;
@@ -18,7 +24,6 @@ function skillDateInterval(){
 	var ho = Math.floor(nap/30);
 	nap = nap%30;
 	var elem = document.getElementById("skilldateinterval");
-	elem.innerHTML = "Nem tudom mi a baj";
 	var datum = " A honlapot fejlesztem:<br>" + ev + " éve " + ho + " hónapja " + nap + " napja";
 	elem.innerHTML = datum;
 	if((nap%2)===0){
@@ -27,4 +32,8 @@ function skillDateInterval(){
 	else{
 		elem.style.color = "#678A1F";
 	}
+}
+	catch(err){
+	alert("Baj van a skillDateInterval() függvénnyel: " + err.message);
+}
 }
