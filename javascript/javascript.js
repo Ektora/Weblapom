17aea4c7e7303a77820a75c4bb814ef6041d82b3
Ektora <zsoltlucski@gmail.com>
2020-11-19 14:57:52 +0100

    Javascript osztály hozzáadása és kiprobálása

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,9 @@
-window.onload=function(){
+"use strict";
+function adatbetoltes(){
 	skillDateInterval();
-skillDate();
+	skillDate();
+	kitolt();
+	
 };
 function skillDate(){
 	try{
@@ -36,4 +39,42 @@ function skillDateInterval(){
 	catch(err){
 	alert("Baj van a skillDateInterval() függvénnyel: " + err.message);
 }
+}
+
+class Animandgard{
+	constructor(sorszam,name,year){
+	this.sorszam=sorszam;
+	this.name=name;
+	this.year=year;
+	}
+	age(){
+	let date = new Date();
+	alert(date.getFullYear() - this.year);
+	return date.getFullYear() - this.year;
+	}
+}
+
+var animal1, animal2, animal3, animal4, animal5, animal6, animal7, animal8;
+	animal1= new Animandgard(1,"Mazsola",2007);
+	animal2= new Animandgard(2,"Burkus",2013);
+	animal3= new Animandgard(3,"Vakarcs",2020);
+	animal4= new Animandgard(4,"Mirci",2017);
+	animal5= new Animandgard(5,"Tigris",2018);
+	animal6= new Animandgard(6,"Bolyhos",2018);
+	animal7= new Animandgard(7,"Falánk",2019);
+	animal8= new Animandgard(8,"Zéró",2019);
+
+function kitolt(){
+	var nevek, korok;
+	try{
+	nevek = document.getElementById("animname1");
+	korok = document.getElementById("animage1");
+	nevek.innerHTML = animal1.name;
+	korok.innerHTML = "Kor: " + animal1.age() + " év";
+	}
+	catch(err){
+	alert(err.message);
+	}
+	
+	
 }
